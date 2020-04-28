5de18f0f4e4b73e2695c64b25928633417de440e
qwert19981228 <34954566+qwert19981228@users.noreply.github.com>
2020-04-28 17:30:55 +0800

    Update MainActivity.kt

--- app/src/main/java/com/example/uiwidgettest/MainActivity.kt
+++ app/src/main/java/com/example/uiwidgettest/MainActivity.kt
@@ -2,11 +2,20 @@ package com.example.uiwidgettest
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.TextView
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        button.setOnClickListener {
+            val inputText = editText.text.toString()
+            Toast.makeText(this,inputText,Toast.LENGTH_SHORT).show()
+            imageView.setImageResource(R.drawable.img_2)
+        }
     }
 }
