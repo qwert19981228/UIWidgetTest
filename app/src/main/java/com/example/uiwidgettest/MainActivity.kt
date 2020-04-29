7f0801677180177fdd91c917b72702bd74676e78
qwert19981228 <34954566+qwert19981228@users.noreply.github.com>
2020-04-29 11:57:26 +0800

    Update MainActivity.kt

--- app/src/main/java/com/example/uiwidgettest/MainActivity.kt
+++ app/src/main/java/com/example/uiwidgettest/MainActivity.kt
@@ -3,8 +3,10 @@ package com.example.uiwidgettest
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.View
 import android.widget.TextView
 import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -16,6 +18,24 @@ class MainActivity : AppCompatActivity() {
             val inputText = editText.text.toString()
             Toast.makeText(this,inputText,Toast.LENGTH_SHORT).show()
             imageView.setImageResource(R.drawable.img_2)
+//            if (progressBar.visibility == View.VISIBLE) {
+//                progressBar.visibility = View.GONE
+//            } else {
+//                progressBar.visibility = View.VISIBLE
+//            }
+            progressBar.progress = progressBar.progress + 10
+            AlertDialog.Builder(this).apply {
+                setTitle("This is Dialog")
+                setMessage("Something important")
+                setCancelable(false)
+                setPositiveButton("OK"){
+                    dialog, which ->
+                }
+                setNegativeButton("Cancel"){
+                    dialog, which ->
+                }
+                show()
+            }
         }
     }
 }
